c7760db45f5178978dc19f4566923b0450b4b682
brunolimafogas <bruno.lima@fogas.com.br>
2025-12-29 18:29:34 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ provider "mongodbatlas" {
 }
 
 resource "mongodbatlas_project" "project" {
-  name   = "projeto-terraform-pro-1"
+  name   = "projeto-terraform-pro"
   org_id = "6909333add5c232d0e778bfa"
 }
 
